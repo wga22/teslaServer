135f4b19e9ed86006cc0bc88822740779ea5f626
wga22 <wga22@yahoo.com>
2017-10-29 12:53:55 -0400

    loadstats update to mongo

--- misc/install_tesla.sh
+++ misc/install_tesla.sh
@@ -1,15 +1,14 @@
 #!/bin/sh -e
 # automated update of nhlpi
 # USAGE
-#		curl -sL https://raw.githubusercontent.com/wga22/nodejs/master/nhl/misc/update-nhlpi.sh | sudo -E bash -
+#		curl -sL https://raw.githubusercontent.com/wga22/teslaServer/master/misc/install_tesla.sh | sudo -E bash -
 if ping -q -c 1 -W 1 google.com >/dev/null; then
 	mkdir /opt/tesla
 	cd /opt/tesla
 	npm install teslams
-	wget -O /opt/tesla/teslams.js https://raw.githubusercontent.com/wga22/nodejs/master/tesla/teslams.js
-	wget -O /opt/tesla/will_tesla.js https://raw.githubusercontent.com/wga22/nodejs/master/tesla/will_tesla.js
-	wget -O /opt/tesla/tesla_battery_levels.js https://raw.githubusercontent.com/wga22/nodejs/master/tesla/tesla_battery_levels.js
-	mkdir /var/log/will_progs/
-	
+	wget -O /opt/tesla/teslams.js https://raw.githubusercontent.com/wga22/teslaServer/master/teslams.js
+	wget -O /opt/tesla/will_tesla.js https://raw.githubusercontent.com/wga22/teslaServer/master/will_tesla.js
+	wget -O /opt/tesla/tesla_battery_levels.js https://raw.githubusercontent.com/wga22/teslaServer/master/tesla_battery_levels.js
+	#mkdir /var/log/will_progs/
 fi
 exit 0
