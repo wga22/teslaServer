89e64d5518076fb3160b87670e0eebc24e59ba18
wga22 <wga22@yahoo.com>
2017-10-29 13:08:05 -0400

    nocache
    --no-cache

--- misc/install_tesla.sh
+++ misc/install_tesla.sh
@@ -9,9 +9,9 @@ if ping -q -c 1 -W 1 google.com >/dev/null; then
 	fi
 	cd $TES_DIR
 	#npm install teslams
-	wget -O $TES_DIR/teslams.js https://raw.githubusercontent.com/wga22/teslaServer/master/teslams.js
-	wget -O $TES_DIR/will_tesla.js https://raw.githubusercontent.com/wga22/teslaServer/master/will_tesla.js
-	wget -O $TES_DIR/tesla_battery_levels.js https://raw.githubusercontent.com/wga22/teslaServer/master/tesla_battery_levels.js
+	wget --no-cache -O $TES_DIR/teslams.js https://raw.githubusercontent.com/wga22/teslaServer/master/teslams.js
+	wget --no-cache -O $TES_DIR/will_tesla.js https://raw.githubusercontent.com/wga22/teslaServer/master/will_tesla.js
+	wget --no-cache -O $TES_DIR/tesla_battery_levels.js https://raw.githubusercontent.com/wga22/teslaServer/master/tesla_battery_levels.js
 	#mkdir /var/log/will_progs/
 fi
 exit 0
